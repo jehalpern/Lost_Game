b88239fc43ea192ad9dfe9ce105512b46c80c549
jehalpern <99103197+jehalpern@users.noreply.github.com>
2022-04-04 17:01:38 -0600

    Initial commit

---
+++ lost.py
@@ -0,0 +1,29 @@
+class Level(object):
+
+    def prompt(self):
+        pass
+
+    def response(self):
+        pass
+
+    def next(self):
+        pass
+
+class Engine(object):
+
+    def __init__ (self, scene):
+        pass
+
+    def play(self):
+        pass
+
+class Tree(object):
+
+    def prompt(self):
+        pass
+
+    def response(self):
+        pass
+
+    def next(self):
+        pass
